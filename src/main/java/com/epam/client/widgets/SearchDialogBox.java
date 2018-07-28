0dae45f1441337976ef8267649b0ccb6c33952a4
denklewer <denklewer@gmail.com>
2018-07-28 15:33:33 +0300

    some changes

--- src/main/java/com/epam/client/widgets/SearchDialogBox.java
+++ src/main/java/com/epam/client/widgets/SearchDialogBox.java
@@ -1,4 +1,36 @@
-package com.epam.client;
+package com.epam.client.widgets;
 
-public class SearchDialogBox {
+import com.google.gwt.user.client.ui.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class SearchDialogBox  extends DialogBox {
+
+    List<String> operations = Arrays.asList("UserProfile");
+
+    public SearchDialogBox(Consumer<String> onFindButtonAction){
+        setText("Search popup");
+
+        setAnimationEnabled(true);
+        VerticalPanel dialogVPanel = new VerticalPanel();
+        dialogVPanel.setSpacing(6);
+        dialogVPanel.addStyleName("dialogVPanel");
+
+        ListBox listBox = new ListBox();
+
+        operations.forEach(listBox::addItem);
+        dialogVPanel.add(listBox);
+
+        TextBox argumentField = new TextBox();
+        dialogVPanel.add(argumentField);
+
+        final Button buttonFind = new Button("Find");
+        buttonFind.addClickHandler(clickEvent -> onFindButtonAction.accept(argumentField.getText()));
+
+        buttonFind.getElement().setId("findButton");
+        dialogVPanel.add(buttonFind);
+        setWidget(dialogVPanel);
+    }
 }
